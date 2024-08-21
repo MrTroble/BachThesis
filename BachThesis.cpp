ed544e641e5eb0a430fd8489ef77cdf49b3610c9
Nico Froehlich <nicofroehlich@icloud.com>
2024-08-21 19:32:48 +0200

    feat: cmake init

---
+++ BachThesis.cpp
@@ -0,0 +1,12 @@
+﻿// BachThesis.cpp : Defines the entry point for the application.
+//
+
+#include "BachThesis.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
